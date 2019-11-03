527cf42ef50d61f4d70b9e32f7188c8ad5d64eb3
Fabiha Ahmed <fsafabiha@gmail.com>
2019-11-02 22:35:18 -0400

    added tornado

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn trivia:app
+web: python trivia/app.py
--- trivia/app.py
+++ trivia/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
 app = Flask(__name__)
 
-#DIR = path.dirname(__file__)
 
 @app.route('/')
 @app.route('/index')
@@ -10,5 +13,10 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
+#    app.debug = False
+#    app.run()
+
+
